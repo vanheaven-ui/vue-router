3e82f2c3575d0e0747b0f9047ac2392af1194e9d
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-09-01 23:40:21 -0700

    Add data to the Jobs view

--- src/views/Jobs.vue
+++ src/views/Jobs.vue
@@ -1,9 +1,22 @@
 <template>
   <h1>Jobs page</h1>
+  <div v-for="job in jobs" :key="job.id">
+    <h3>{{ job.title }}</h3>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      jobs: [
+        { id: 1, title: "UX Designer", details: "Lorem" },
+        { id: 2, title: "React Developer", details: "Lorem" },
+        { id: 3, title: "Vue Developer", details: "Lorem" },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
