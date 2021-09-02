6d3f2375bb34a3fb93ca12df153fed0f8e31b190
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-09-02 00:10:50 -0700

    Add programmatic redirecting using

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,28 @@
     <router-link :to="{ name: 'About' }">About</router-link> |
     <router-link :to="{ name: 'Jobs' }">Jobs</router-link>
   </div>
+
+  <button @click="redirect">Redirect</button>
+  <button @click="back">Back</button>
+  <button @click="forward">Forward</button>
   <router-view />
 </template>
 
+<script>
+export default {
+  methods: {
+    redirect() {
+      this.$router.push({ name: "Home" });
+    },
+    back() {
+      this.$router.go(-1);
+    },
+    forward() {
+      this.$router.go(1);
+    },
+  },
+};
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
