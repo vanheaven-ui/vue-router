5d4b37651b70e25332cb7f604307a4e5e8530d1f
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-09-02 23:44:16 -0700

    fetch and render received data from APi

--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -14,6 +14,11 @@ export default {
       jobs: [],
     };
   },
+  mounted() {
+    fetch("http://localhost:3000/jobs")
+      .then((res) => res.json())
+      .then((data) => (this.jobs = data));
+  },
 };
 </script>
 
